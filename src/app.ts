e08c54a23e000d782b21afdb56e3054adcef36fa
Makdoom Shaikh <makshaikh99@gmail.com>
2024-08-16 22:56:05 +0530

    post request

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,14 @@ const app = express();
 
 app.use(morganMiddleware);
 app.use(cors());
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.json());
 
 app.get("/", (_, res) => res.send("✅ It is really working !"));
+
+app.post("/test", (req, res) => {
+  const body = req.body;
+  res.json({ success: 1, body });
+});
 
 export default app;
